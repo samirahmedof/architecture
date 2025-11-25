a9c7a91383cee8dab73a11b7a36169e1363f9352
samirahmedof <samirna@code.edu.az>
2025-11-25 16:09:04 +0400

    test

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+export default {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        // Məsələn: 'type-enum' qaydasını bir az yumşalda və ya sərtləşdirə bilərsən
+        // 'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
+    },
+};
